8f4de0be9c2593d9a9ddb5e5897478be2e36a81b
RubinVelcani <rubin.velcani@gmail.com>
2022-10-04 10:25:23 +0200

    'Homepage done 35%'

---
+++ scripts/menu.js
@@ -0,0 +1,21 @@
+/* MENU */
+let burgerMenu = document.getElementById('burger-menu');
+
+let overlay = document.getElementById('menu-wrapper');
+
+let navButtons = document.getElementsByClassName('nav-button');
+let socialsButtons = document.getElementsByClassName('socials-button');
+
+function eventListenerAdder (button) {
+  button.addEventListener('click',function(){
+    burgerMenu.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+  });
+}
+
+eventListenerAdder(burgerMenu);
+
+
+for(let i = 0; i < navButtons.length; i++) {
+  eventListenerAdder(navButtons[i]);
+}
